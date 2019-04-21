41d1a06d232b9dcdff2d233e297955d62146845f
Armen Badalyan <flexer87@gmail.com>
2019-04-22 02:23:26 +0300

    moved pagination state to a separate store

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,17 +3,20 @@ import { EventStore } from "./eventStore";
 import { EventDetailsStore } from "./eventDetailsStore";
 import AuthService from "../services/authService";
 import EventService from "../services/eventService";
+import { EventPaginationStore } from "./eventPaginationStore";
 
 export default function initStores() {
   const authService = new AuthService();
   const eventService = new EventService();
   const authStore = new AuthStore(authService);
+  const eventPaginationStore = new EventPaginationStore();
   const eventStore = new EventStore(eventService);
   const eventDetailsStore = new EventDetailsStore(eventStore);
 
   return {
     authStore,
     eventStore,
-    eventDetailsStore
+    eventDetailsStore,
+    eventPaginationStore
   };
 }
